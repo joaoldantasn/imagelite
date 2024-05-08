bfa08f8f7800df9c48f52e32df5e3ece02b5300d
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-05-08 10:33:54 -0300

    ImageCard

--- imagelite/imagelite/src/app/galeria/page.tsx
+++ imagelite/imagelite/src/app/galeria/page.tsx
@@ -1,9 +1,17 @@
-import {Template} from '@/components'
+import {Template, ImageCard} from '@/components'
 
 export default function GaleriaPage(){
   return(
     <Template>
       <h1>Galeria</h1>
+      <section className='grid grid-cols-3 gap-8'>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+      </section>
     </Template>
   )
 }
