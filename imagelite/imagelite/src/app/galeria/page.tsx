31eb745fffb5d471d20b5b6a4fd1a18f9f3ccfb6
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-05-08 14:30:50 -0300

    atualizado

--- imagelite/imagelite/src/app/galeria/page.tsx
+++ imagelite/imagelite/src/app/galeria/page.tsx
@@ -1,16 +1,50 @@
+'use client'
+
 import {Template, ImageCard} from '@/components'
+import {Image} from '../resources/image/image.resource'
+import {useImageService} from '../resources/image/image.service'
+import { useState } from 'react'
 
 export default function GaleriaPage(){
+
+  const useService = useImageService();
+  const [images, setImages] = useState<Image[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const [extension, setExtension] = useState<string>('');
+
+  async function searchImages(){
+    const result = await useService.buscar();
+    setImages(result);
+  }
+
+  function renderImageCard(image: Image){
+    return(
+      <ImageCard key={image.url} nome={image.name} src={image.url} tamanho={image.size} dataUpload={image.uploadDate}/>
+    )
+  }
+
+  function renderImageCards(){
+    return images.map(renderImageCard)
+  }
+
   return(
     <Template>
-      <h1>Galeria</h1>
+      <section className='flex flex-col items-center justify-center my-5'>
+        <div className='flex space-x-4'>
+          <input type="text" onChange={event => setQuery(event.target.value)} className='border px-5 py-2 rounded-lg text-gray-900'/>
+          <select onChange={event => setExtension(event.target.value)} className='border px-4 py-2 rounded-lg text-gray-900'>
+            <option value="">All formats</option>
+            <option value="PNG">PNG</option>
+            <option value="JPEG">JPEG</option>
+            <option value="GIF">GIF</option>
+          </select>
+          <button className='bg-blue-500 text-white px-4 py-2 rounded-lg' onClick={searchImages}>Search</button>
+          <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg'>Add New</button>
+        </div>
+      </section>
+      
       <section className='grid grid-cols-3 gap-8'>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
-        <ImageCard nome='Montanha' tamanho="10MB" src="https://images.unsplash.com/photo-1618588507085-c79565432917?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww"/>
+        {renderImageCards()}
       </section>
     </Template>
   )
--- imagelite/imagelite/src/app/resources/image/image.service.ts
+++ imagelite/imagelite/src/app/resources/image/image.service.ts
@@ -4,10 +4,11 @@ import {Image} from './image.resource'
 class ImageService {
   baseURL: string = 'http://localhost:8080/v1/images';
 
-  async buscar(): Promise<Image[]> {
-    const response = await fetch(this.baseURL);
+  async buscar(query: string = "", extension: string = ""): Promise<Image[]> {
+    const url = `${this.baseURL}?query=${query}&extension=${extension}`
+    const response = await fetch(url);
     return await response.json();
   }
 }
-
+//react hook
 export const useImageService = () => new ImageService();
