70459cd84f3cdddc1222531dcffd866c2957ff0f
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-05-08 21:32:37 -0300

    melhorias no card

--- imagelite/imagelite/src/components/ImageCard.tsx
+++ imagelite/imagelite/src/components/ImageCard.tsx
@@ -2,12 +2,13 @@
 
 interface ImageCardProps {
   nome?: string;
-  tamanho ?: string;
+  tamanho ?: number;
   dataUpload ?: string;
   src ?: string;
+  extension ?: string;
 }
 
-export const ImageCard: React.FC<ImageCardProps> = ({nome, dataUpload, src, tamanho}: ImageCardProps) => {
+export const ImageCard: React.FC<ImageCardProps> = ({nome, dataUpload, src, tamanho, extension}: ImageCardProps) => {
   function download(){
     window.open(src, '_blank')
   }
@@ -17,9 +18,22 @@ export const ImageCard: React.FC<ImageCardProps> = ({nome, dataUpload, src, tama
       <img onClick={download} src={src} className="h-56 w-full object-cover rounded-t-md" alt=""/>
       <div className="card-body p-4">
         <h5 className="text-x1 font-semibold mb-2 text-gray-600">{nome}</h5>
-        <p className="text-gray-600">{tamanho}</p>
+        <p className="text-gray-600">{extension}</p>
+        <p className="text-gray-600">{formatBytes(tamanho)}</p>
         <p className="text-gray-600">{dataUpload}</p>
       </div>
     </div>
   )
+}
+
+function formatBytes(bytes: number = 0, decimals = 2) {
+  if (!+bytes) return '0 Bytes'
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
